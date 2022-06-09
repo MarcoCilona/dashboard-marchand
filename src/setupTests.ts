d33de70284b3872068cfde6c625f79fb67abb830
MarcoCilona <marco.cilona@gmail.com>
2022-06-10 00:55:56 +0200

    Added mock for i18n test

--- src/setupTests.ts
+++ src/setupTests.ts
@@ -5,3 +5,15 @@
 import '@testing-library/jest-dom';
 
 global.ResizeObserver = require('resize-observer-polyfill');
+
+jest.mock('react-i18next', () => ({
+  // this mock makes sure any components using the translate hook can use it without a warning being shown
+  useTranslation: () => {
+    return {
+      t: (str) => str,
+      i18n: {
+        changeLanguage: () => new Promise(() => null),
+      },
+    };
+  },
+}));
