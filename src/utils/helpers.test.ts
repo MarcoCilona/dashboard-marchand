24f49dbe23d3e12d52a5b489fe64174699ef086b
MarcoCilona <marco.cilona@gmail.com>
2022-06-10 13:44:49 +0200

    Added test for removeDuplicates helper

--- src/utils/helpers.test.ts
+++ src/utils/helpers.test.ts
@@ -1,26 +1,33 @@
-import { capitalizeFirstLetter, currencyPrefix, formatWithoutSymbols, sortArray } from './helpers';
+import exp from 'constants';
+import {
+  capitalizeFirstLetter,
+  currencyPrefix,
+  formatWithoutSymbols,
+  removeDuplicates,
+  sortArray,
+} from './helpers';
 
 describe('Helpers utils testing', () => {
-  it('Test if, given a string, the utils returns a string with first letter capitalized', () => {
+  it('capitalizeFirstLetter - Test if, given a string, the utils returns a string with first letter capitalized', () => {
     const originalString = 'test string';
 
     expect(capitalizeFirstLetter(originalString)).toBe('Test string');
   });
 
-  it('Testing method that, given a string, returns it with a currency prefix', () => {
+  it('currencyPrefix - Testing method that, given a string, returns it with a currency prefix', () => {
     const originalString = '3456';
 
     expect(currencyPrefix({ value: originalString })).toBe('$3456');
     expect(currencyPrefix({ value: originalString, currency: '€' })).toBe('€3456');
   });
 
-  it('Testing method that, given a string, returns it formatted without _', () => {
+  it('formatWithoutSymbols- Testing method that, given a string, returns it formatted without _', () => {
     const originalString = 'ab_cd';
 
     expect(formatWithoutSymbols(originalString)).toBe('ab cd');
   });
 
-  it('Testing sort array of objects by key', () => {
+  it('sortArray - Testing sort array of objects by key', () => {
     const shuffledArray = [
       {
         name: 'd',
@@ -48,5 +55,12 @@ describe('Helpers utils testing', () => {
     ];
 
     expect(sortedArray).toEqual(expectedOutcome);
+  });
+
+  it('removeDuplicates - Testing that, given an array with duplicates in it, removes them', () => {
+    const originalArray = ['a', 'b', 'b', 'c', 'd', 'd', 'b'];
+    const expectedOutcome = ['a', 'b', 'c', 'd'];
+
+    expect(removeDuplicates(originalArray)).toEqual(expectedOutcome);
   });
 });
